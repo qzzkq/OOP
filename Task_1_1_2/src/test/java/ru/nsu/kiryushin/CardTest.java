779dd390e2600ff43bbfec8f862b63e2232d618c
Ivan <mister.vanya1234@gmail.com>
2025-10-06 00:08:51 +0700

    build test

---
+++ Task_1_1_2/src/test/java/ru/nsu/kiryushin/CardTest.java
@@ -0,0 +1,15 @@
+package ru.nsu.kiryushin;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CardTest{
+
+    @Test
+    void test1(){
+        assertTrue(true);
+    }
+}

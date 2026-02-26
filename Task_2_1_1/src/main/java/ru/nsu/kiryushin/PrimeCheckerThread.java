8d2ae3725ebb8e7f89b0014c35484f6e828be56a
Ivan <mister.vanya1234@gmail.com>
2026-02-26 19:51:16 +0700

    remade graph + edited code

--- Task_2_1_1/src/main/java/ru/nsu/kiryushin/PrimeCheckerThread.java
+++ Task_2_1_1/src/main/java/ru/nsu/kiryushin/PrimeCheckerThread.java
@@ -13,9 +13,15 @@ public class PrimeCheckerThread {
      * @return True if the number is prime, false otherwise
      */
     private static boolean isPrime(int number) {
-        if (number == 1) return false;
-        if (number == 2) return true;
-        if ((number % 2) == 0) return false;
+        if (number == 1) {
+            return false;
+        }
+        if (number == 2){
+            return true;
+        }
+        if ((number % 2) == 0){
+            return false;
+        }
         for (int i = 3; i * i <= number; i += 2) {
             if (number % i == 0) {
                 return false;
